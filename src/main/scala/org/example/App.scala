43768ad8a677ffeecb4b42f1fa46034e7fcd7e3e
Pablo Brianese <pablo.brianese@despegar.com>
2022-11-09 17:05:07 -0300

    Lectura del conjunto de datos

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -1,18 +1,32 @@
-package org.example
-import org.apache.spark.sql.SparkSession
-
 /**
  * @author ${user.name}
  */
-object SparkExample extends App {
+package org.example
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructField, StructType}
 
+object EXGBoostExample extends App {
   val spark = SparkSession.builder()
-    .master("local[1]")
-    .appName("SparkExample")
+    .master(master = "local[1]")
+    .appName(name = "XGBoostExample")
     .getOrCreate()
-  
-  println("First SparkContext")
-  println("APP Name :" + spark.sparkContext.appName)
-  println("Deploy Mode :" + spark.sparkContext.deployMode)
-  println("Master :" + spark.sparkContext.master)
+  val schema = new StructType(
+    Array(
+      StructField("index", IntegerType, false),
+      StructField("sepal length (cm)", DoubleType, false),
+      StructField("sepal width (cm)", DoubleType, false),
+      StructField("petal length (cm)", DoubleType, false),
+      StructField("petal width (cm)", DoubleType, false),
+      StructField("species", StringType, false)
+    )
+  )
+  val rawInput = spark.read
+    .schema(schema = schema)
+    .option("header", true)
+    .option("delimiter", ",")
+    .option("quotes", "")
+    .csv(path = "data/iris.csv")
+
+  rawInput.printSchema()
+  rawInput.show()
 }
